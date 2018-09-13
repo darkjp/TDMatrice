8de024f8e6a4405cee08852220aeddd80b6dd8db
darkjp <31925255+darkjp@users.noreply.github.com>
2018-09-13 23:49:40 +0200

    Update AbstractMatrice.java

--- src/AbstractMatrice.java
+++ src/AbstractMatrice.java
@@ -1,36 +1,46 @@
-import java.awt.Point;
-
 public abstract class AbstractMatrice<T> implements IMatrice<T> {
-    private int mLigne;
-    private int mColonne;
-    private T mValue;;
-    
-	public AbstractMatrice (int ligne, int colonne, T t) {
-		mLigne = ligne; 
+	private int mLigne;
+	private int mColonne;
+	private T mValue;
+
+	public AbstractMatrice(int ligne, int colonne, T t) {
+		mLigne = ligne;
 		mColonne = colonne;
 		mValue = t;
-		}
-
+	}
 
 	public int getLigne() {
-	   return mLigne;   
+		return mLigne;
 	}
-	
-	public int getColonne(){
-	    return mColonne;
+
+	public int getColonne() {
+		return mColonne;
 	}
-		
-	public String toString () {
-	   	StringBuilder strResult = new StringBuilder();
+
+	public T ReadIndice(int ligneI, int colonneJ) {
+		return mValue;
+	}
+
+	public T setIndice(int ligneI, int colonneJ, T t) {
+		return mValue = t;
+	}
+
+	public String strResult() {
+		StringBuilder strResult = new StringBuilder();
 		for (int i = 0; i < getLigne(); ++i) {
 			for (int j = 0; j < getColonne(); ++j) {
-			    if (j == getColonne())
-			         str.append("E" + getIndice(i, j) + " \n");
-				else 
-                    strResult.append("E" + getIndice(i, j));
+				if (j == getColonne())
+					strResult.append("E" + ReadIndice(i, j) + " \n");
+				else
+					strResult.append("E" + ReadIndice(i, j));
 			}
-
-	    return (String)strResult;
+		}
+		return strResult.toString();
 	}
-	
+
+	public String toString(String s) {
+		return s;
+
+	}
+
 }
