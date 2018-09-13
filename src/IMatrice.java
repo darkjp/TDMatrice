bf614028b9253389c30bd5c440ee33e888205cc3
darkjp <31925255+darkjp@users.noreply.github.com>
2018-09-13 23:50:02 +0200

    Update IMatrice.java

--- src/IMatrice.java
+++ src/IMatrice.java
@@ -1,9 +1,10 @@
-import java.awt.Point;
+public interface IMatrice<T> {
 
+	public int getLigne();
 
-public interface IMatrice {
-    public int getLignes();
-	public int getColonnes();
-	public Point getIndice(int ligneI, int colonneJ);
-	public int setIndice(int colonneJ);
+	public int getColonne();
+
+	public T ReadIndice(int ligneI, int colonneJ);
+
+	public T setIndice(int ligneI, int colonneJ, T t);
 }
