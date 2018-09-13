dcd1d1f5a1ffbedf5cf25713e5647544c67a4ba3
darkjp <31925255+darkjp@users.noreply.github.com>
2018-09-14 00:41:36 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
 	public static void main(String args[]) {
 
-		IMatrice<Integer> mint = new MatriceTab<>(4, 4, 0);
+		IMatrice<Integer> mint = new MatriceTab<>(6, 6, 0);
+		int i, j, ii, jj;
+		int valeur = 0;
+		int valeur2 = mint.getColonne();
+		
+		for (i = 0; i < mint.getLigne(); i++)
+			for (j = 0; j < mint.getColonne(); j++) {
+				if (i <= j)
+					mint.setIndice(i, j, valeur++);
+				else mint.setIndice(i, j, valeur2--);
+			}
+					
+		
 		IMatrice<String> mstring = new MatriceTab<>(4, 4, "");
 
 		System.out.println(mint.toString());
